c922614437dbd91fa45f46959e13048099431e1b
JungHoiMin <wony1102@naver.com>
2022-05-30 22:22:18 +0900

    각 VO 설정

--- Cloud-IDE-Web-Application/src/main/java/com/tukorea/ciwa/domain/FileVO.java
+++ Cloud-IDE-Web-Application/src/main/java/com/tukorea/ciwa/domain/FileVO.java
@@ -1,5 +1,45 @@
 package com.tukorea.ciwa.domain;
 
 public class FileVO {
+	private String title;
+	private String userid;
+	private String type;
+	private String modifyDate;
 
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getUserid() {
+		return userid;
+	}
+
+	public void setUserid(String userid) {
+		this.userid = userid;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getModifyDate() {
+		return modifyDate;
+	}
+
+	public void setModifyDate(String modifyDate) {
+		this.modifyDate = modifyDate;
+	}
+
+	@Override
+	public String toString() {
+		return "FileVO [title=" + title + ", userid=" + userid + ", type=" + type + ", modifyDate=" + modifyDate + "]";
+	}
 }
